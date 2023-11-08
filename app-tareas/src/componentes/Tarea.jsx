a0477b7e5a12054903cff606e84fcce6de63b678
Catalina Arriagada <catalina.arriagada1@gmail.com>
2023-11-08 16:59:41 -0300

    front-app

---
+++ app-tareas/src/componentes/Tarea.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../hojas-de-estilo/Tarea.css';
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+function Tarea({ id, texto, completada, completarTarea, eliminarTarea }) {
+  return (
+    <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
+      <div 
+        className="tarea-texto"
+        onClick={() => completarTarea(id)}>
+        {texto}
+      </div>
+      <div className="tarea-icono"
+            onClick={() => eliminarTarea(id)}>
+        <AiOutlineCloseCircle className='tarea-contenedor-iconos' /> 
+      </div>
+    </div>
+  );
+}   
+
+export default Tarea;
+//ctrl+shift+P -> indent using spaces
---
+++ app-tareas/src/componentes/TareaForm.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import '../hojas-de-estilo/TareaForm.css'
+import { v4 as uuidv4 } from 'uuid';
+
+function TareaForm(props){
+
+    const [input, setInput] = useState('');
+
+    const manejarCambio = e => {
+        setInput(e.target.value);
+    };
+
+    const manejarEnvio = e => {
+        e.preventDefault();
+        const tareaNueva = {
+            id: uuidv4(),
+            texto: input,
+            completada: false
+        };
+
+        props.onSubmit(tareaNueva);
+    };
+
+    return (
+       <form action="" 
+            className="tarea-formulario"
+            onSubmit={manejarEnvio}>
+            <input type="text" 
+                className="tarea-input" 
+                placeholder='Escribe una tarea'
+                name='texto'
+                onChange={manejarCambio}
+                />
+            <button className="tarea-boton">
+                Agregar Tarea
+            </button>
+       </form> 
+    );
+}
+
+export default TareaForm;
